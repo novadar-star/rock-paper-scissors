4713022277b78d6f6c4a7cbfb866044157c6ee6e
Darla Nova <jolynekujoismywife06@gmail.com>
2025-07-31 04:19:51 +0800

    create playround function by comparing strings and counting their score

--- game.js
+++ game.js
@@ -23,3 +23,42 @@ function getHumanChoice(){
     console.log(input.toLowerCase());
 }
 getHumanChoice();
+
+
+//tracker of human and computer score
+//compare the strings if the same
+//if same display "Rock beats paper "
+//increment
+
+let humanScore = 0; let computerScore = 0;
+function playRound(humanChoice, computerChoice){
+    if(humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("rock beats paper");
+        humanScore++;
+    }
+    else if(humanChoice == "paper" && computerChoice == "rock"){
+        console.log("paper beats rock");
+        humanScore ++
+    }
+    else if(humanChoice == "scissors" && computerChoice =="paper" ){
+        console.log("scissors beats paper");
+        humanScore++;
+    }
+    // rock
+    else if(computerChoice== "rock" && humanChoice == "scissors"){
+        console.log("rock beats paper");
+        computerScore++;
+    }
+    else if(computerChoice == "paper" && humanChoice == "rock"){
+        console.log("paper beats rock");
+        computerScore++;
+
+    }
+    else{
+        console.log("scissors beats paper");
+        computerScore++;
+    }
+
+}
+
+playRound(humanSelection, computerSelection);
