5e49fe16725271fecea03a5f85abfdb9fe95e8f6
Darla Nova <jolynekujoismywife06@gmail.com>
2025-08-12 09:53:19 +0800

    Added score text, but not functional yet

--- game.js
+++ game.js
@@ -7,24 +7,31 @@ function getComputerChoice(){
 
     switch(index){
         case 0:
-          computerChoice.classList.add("computerChoice");
-          computerChoice.textContent =  "rock";
-         cont.appendChild(computerChoice);  
-         return "rock";
+        computerChoice.classList.add("computerChoice");
+        computerChoice.textContent =  "rock";
+        cont.appendChild(computerChoice);  
+        return "rock";
+
         case 1:
-          computerChoice.classList.add("computerChoice");
-          computerChoice.textContent =  "paper";
-          cont.appendChild(computerChoice);    
-          return "paper";  
+        computerChoice.classList.add("computerChoice");
+        computerChoice.textContent =  "paper";
+        cont.appendChild(computerChoice);    
+        return "paper";  
+
         case 2:
-          computerChoice.classList.add("computerChoice");
-          computerChoice.textContent =  "scissors";
-          cont.appendChild(computerChoice);     
-          return "scissors"; 
+        computerChoice.classList.add("computerChoice");
+        computerChoice.textContent =  "scissors";
+        cont.appendChild(computerChoice);     
+        return "scissors"; 
          
     }
    
 }
+const score = document.querySelector(".score");
+
+let humanScore=0;
+let computerScore = 0;
+let round = "";
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice === "rock" && computerChoice === "scissors"){
@@ -60,24 +67,29 @@ function playRound(humanChoice, computerChoice){
         humanScore++;
         return "tie!";
     }
-   
 }
 
-/*
+//score.textContent
+
 function playGame(){ //invokes function 5times
+    const score = document.querySelector(".score");
+    const scoretext = document.createElement("h4");
    
-   // for(let i=1; i<=5; i++){
+    scoretext.classList.add("scoretext");
+    scoretext.textContent = humanScore;
+    score.appendChild(scoretext);  
+   // for(let i=1; i<=5; i++)
       //  console.log(`Round ${i}`);
         console.log(playRound());
-        console.log(`Human choice: ${humanChoice} | score: ${humanScore}`);
-        console.log(`Computer choice: ${computerChoice} | score: ${computerScore}`);
+        console.log(`Human choice: | score: ${humanScore}`);
+        console.log(`Computer choice: | score: ${computerScore}`);
         console.log("========================");
    // }
   winner();
 }
- */
+
     
-/*
+
 function winner(){
          if(humanScore === computerScore){
             console.log("HUMAN AND COMPUTER TIE!");
@@ -90,20 +102,17 @@ function winner(){
         }
 }
 
-*/
-//playGame();
+
+playGame();
 
 
-let humanScore=0;
-let computerScore = 0;
-let round = "";
-let roundcount = 0;
 
 const btnChoice = document.querySelectorAll("button");
 
-for(let indexx = 0; indexx< btnChoice.length;indexx+=1){
+for(let indexx = 0; indexx< btnChoice.length;indexx++){
 btnChoice[indexx].addEventListener("click", pressBtns);
 function pressBtns(e) {
+    playRound();
     let target = e.target;
     const cont = document.querySelector(".steve");
     const humanChoice = document.createElement("h4");
@@ -116,26 +125,22 @@ function pressBtns(e) {
         humanChoice.textContent = "rock";
         cont.appendChild(humanChoice);   
         round = playRound("rock", computerChoice);
-        roundcount++;
         return "rock";
         
         case "paper":
         humanChoice.textContent = "paper";
         cont.appendChild(humanChoice);   
         round = playRound("paper", computerChoice);
-        roundcount++;
         return "paper";
 
         case "scissors":
          humanChoice.textContent = "scissors";
         cont.appendChild(humanChoice);   
         round = playRound("scissors", computerChoice);
-        roundcount++;
         return "scissors";
   
     }
     console.log(round);
 } 
 }
-//create an p element under steve similar to zombie so dun ilalagay 
-//pinindot mong option
+
