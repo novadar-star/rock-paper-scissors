eed796134765a28bd00ee26cbef9a5738352067d
Darla Nova <jolynekujoismywife06@gmail.com>
2025-08-14 20:28:36 +0800

    shorten the if-else statment of choices

--- game.js
+++ game.js
@@ -2,32 +2,8 @@
 
 //if i dont putonclick ="getcomputerchoice" on buttons html, zombi img doesnt change and di nagrereturn
 //assign zombie container to the random
-    const Img = ["rock.jpg", "paper.jpg", "scissors.jpg"]
+const Img = ["rock.jpg", "paper.jpg", "scissors.jpg"]
 
-function getComputerChoice(){
-    let index =  Math.floor(Math.random() * 3);
-    let zombieResult =  document.querySelector(".zombie img")
-    let weapon = "";
-
-    switch(index){
-        case 0:
-            zombieResult.src = Img[0];
-            weapon = "rock";
-            break;
-        case 1:
-            zombieResult.src = Img[1];
-            weapon = "paper";
-            break;
-        case 2:
-             zombieResult.src = Img[2];
-            weapon = "scissors";
-            break;
-        default:
-            break;
-    }
-    alert("ZOMBIE: " + weapon);
-    return zombieResult, weapon;
-}
 
 function getHumanChoice(){
     let humanResult = document.querySelector(".steve img")
@@ -54,11 +30,37 @@ function getHumanChoice(){
                     break;
 
             }
-            alert("HUMAN: " + input);
+            console.log("HUMAN: " + input);
             return humanResult, input;
 
         })
     })
+}
+
+
+function getComputerChoice(){
+    let index =  Math.floor(Math.random() * 3);
+    let zombieResult =  document.querySelector(".zombie img")
+    let weapon = "";
+
+    switch(index){
+        case 0:
+            zombieResult.src = Img[0];
+            weapon = "rock";
+            break;
+        case 1:
+            zombieResult.src = Img[1];
+            weapon = "paper";
+            break;
+        case 2:
+             zombieResult.src = Img[2];
+            weapon = "scissors";
+            break;
+        default:
+            break;
+    }
+    console.log("ZOMBIE: " + weapon);
+    return zombieResult, weapon;
 }
 
   
@@ -66,53 +68,34 @@ function getHumanChoice(){
 let humanScore=0;
 let computerScore = 0;
 
-  
-function playRound(){
 
-   humanChoice = getHumanChoice();
- 
-//result not diplayin except for tie
-    if(humanChoice === "rock" && computerChoice === "scissors"){
-       //not displaying   
+function playRound(){
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+     let result = "";
+    if(humanChoice === "rock" && computerChoice === "scissors" || 
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice ==="paper" ){
         humanScore++;
-        result = "U WIN rock beats scissors"; 
-     }
-    else if(humanChoice === "paper" && computerChoice === "rock"){
-       
-        humanScore++;
-        result = "U WIN paper beats rock";
-    }
-    else if(humanChoice === "scissors" && computerChoice ==="paper" ){
-        humanScore++;
-         result= "U WIN scissors beats paper";
+        result= (`You win this round ${humanChoice} beats ${computerChoice}`);
     }
     // rock
-    else if(computerChoice === "rock" && humanChoice === "scissors"){
-     
+    else if(computerChoice === "rock" && humanChoice === "scissors" ||
+    computerChoice === "paper" && humanChoice === "rock" ||
+    computerChoice === "scissors" && humanChoice === "paper"){
         computerScore++;
-          result =  "U LOSE rock beats scissors";
-    }
-    else if(computerChoice === "paper" && humanChoice === "rock"){
-    
-        computerScore++;
-         result= "U LOSE paper beats rock";
-    }
-    else if (computerChoice === "scissors" && humanChoice === "paper"){
-   
-        computerScore++;
-         result = "U LOSE scissors beats paper";
+        result= (`You lost this round ${computerChoice} beats ${humanChoice} `);
     }
     else { //if all are tie
-        computerScore++;
-        humanScore++;
          result =  "tie!";
     }
-    return result, computerScore,humanScore;
+    alert("RESULT: " + result); //not outputting wtf
+    return computerScore, humanScore;
    
 }
-   //buttons work but not oputputting anything on console
-   //random always work first..it must be base on user click
+playRound();
 
+/*
 function playGame(){ //invokes function 5times
     let resulttext = document.querySelector(".result");
     for(let i=1; i<=5; i++){
@@ -144,4 +127,4 @@ function winner(){
 }
 
 playGame();
- 
+ */
